e1a84aa27d441575c06ad3dde832c04325df53e8
김준식 <105399710+popcorn0125@users.noreply.github.com>
2025-12-15 00:30:59 +0900

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,13 +5,13 @@ module.exports = defineConfig({
     proxy: {
       '/map': {
         // /map로 들어오면 포트 8080(스프링 서버)로 보냄
-        target: 'https://port-0-meetpoint-backend-1272llwvu36eh.sel5.cloudtype.app',
+        target: 'https://port-0-meetpoint-backend-1272llwvu36eh.sel5.cloudtype.app/',
         // pathRewrite:{'^/':''},
         changeOrigin: true // cross origin 허용
       },
       '/choice': {
         // /choice로 들어오면 포트 8080(스프링 서버)로 보냄
-        target: 'https://port-0-meetpoint-backend-1272llwvu36eh.sel5.cloudtype.app',
+        target: 'https://port-0-meetpoint-backend-1272llwvu36eh.sel5.cloudtype.app/',
         changeOrigin: true // cross origin 허용
       }
     }
