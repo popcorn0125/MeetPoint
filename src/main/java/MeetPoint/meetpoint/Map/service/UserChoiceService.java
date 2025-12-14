0654720f95d08ec4a721896960aeffd1e13a1690
김준식 <rlawnstlr54@gmail.com>
2025-12-14 22:28:00 +0900

    김준식 UserChoiceService.java 카카오톡 공유 Double타입으로 변환 수정완료

--- src/main/java/MeetPoint/meetpoint/Map/service/UserChoiceService.java
+++ src/main/java/MeetPoint/meetpoint/Map/service/UserChoiceService.java
@@ -33,9 +33,9 @@ public class UserChoiceService {
         // 중간지점명, 중간지점 위도와 경도를 저장하고 해당 인덱스 값을 반환( 이유 : 사용자가 선택한 장소들을 저장하기위해 사용 )
         try{
             // 중간 좌표의 위도가 문자열로 저장되어 실수로 변환
-            params.put("mpLat", Double.parseDouble((String) params.get("mpLat")));
+            params.put("mpLat", params.get("mpLat"));
             // 중간 좌표의 경도가 문자열로 저장되어 실수로 변환
-            params.put("mpLon", Double.parseDouble((String) params.get("mpLon")));
+            params.put("mpLon", params.get("mpLon"));
             userChoiceDao.storeMP(params);
             // 저장한 인덱스 반환
             Long idx = (Long) params.get("id");
