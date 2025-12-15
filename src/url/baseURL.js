be2591899fea0b0cd9aa6ce8ef23a1e1c5288da5
김준식 <105399710+popcorn0125@users.noreply.github.com>
2025-12-15 15:39:57 +0900

    Update baseURL.js

--- src/url/baseURL.js
+++ src/url/baseURL.js
@@ -1,7 +1,4 @@
 import axios from "axios";
 
-const baseURL = axios.create({
-    baseURL : 'https://port-0-meetpoint-backend-1272llwvu36eh.sel5.cloudtype.app'
-});
-
+const baseURL = 'https://port-0-meetpoint-backend-1272llwvu36eh.sel5.cloudtype.app';
 export default baseURL;
