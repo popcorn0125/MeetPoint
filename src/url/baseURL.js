7a1c927f4f475c68b60030e240ce7315360ecd22
김준식 <105399710+popcorn0125@users.noreply.github.com>
2025-12-15 16:19:11 +0900

    Update baseURL.js 주소 변경

--- src/url/baseURL.js
+++ src/url/baseURL.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL : "http://localhost:8080",
+    baseURL : 'https://port-0-meetpoint-backend-1272llwvu36eh.sel5.cloudtype.app',
+    // baseURL : "http://localhost:8080",
     headers : {
         'Content-Type': 'application/json; charset=UTF-8' 
     },
