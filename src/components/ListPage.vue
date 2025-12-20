2921b4d5464a219113a2bb160804ccf98cfe9fa8
김준식 <rlawnstlr54@gmail.com>
2025-12-20 23:07:40 +0900

    김준식 카톡 공유

--- src/components/ListPage.vue
+++ src/components/ListPage.vue
@@ -476,7 +476,7 @@ export default {
         },
 
         sharekakao(id) {
-            const url = "http://localhost/ListPage.page?where=" + encodeURI(id) + // DB에 저장된 ID값
+            const url = "https://web-meetpoint-frontend-1272llwvu36eh.sel5.cloudtype.app/ListPage.page?where=" + encodeURI(id) + // DB에 저장된 ID값
                 "&startDate=" + this.selectedStartDate +  // 시작 날짜
                 "&startDay=" + encodeURI(this.selectedStartDay) +  // 시작 요일
                 "&endDate=" + this.selectedEndDate +  // 마지막 날짜
